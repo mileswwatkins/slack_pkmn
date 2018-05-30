ba54e92b2f08331cd45a50b0e372b88d6aae8a91
Miles Watkins <miles.w.watkins@gmail.com>
2018-05-30 00:16:32 -0400

    Switch namespace to hyphen, lint syntax, handle single-GIF download; closes #2

--- get_gifs.sh
+++ get_gifs.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+set -e
+
 PKMN_DIR="pkmn"
-NAMESPACE="pkmn_"
+NAMESPACE="pkmn-"
 MAX_FILESIZE="64000"
 MAX_DIMENSION="128"
 SAFE_DIMENSION="40"
 
 # Download and process GIFs to be ready for Slack
 mkdir -p $PKMN_DIR
-for pkmn_num in `seq -f "%03g" $1 $2`; do
+# Handle the case where only one Pokemon number is entered
+for pkmn_num in $(seq -f "%03g" "$1" "${2:-$1}"); do
 	# Name the emoji properly
-	PKMN_NAME="$(wget --quiet --output-document=- http://pokeapi.co/api/v2/pokemon/${pkmn_num} | jq --raw-output '.name')"
+	PKMN_NAME="$(wget --quiet --output-document=- "http://pokeapi.co/api/v2/pokemon/${pkmn_num}" | jq --raw-output '.name')"
 
-	GIF_LOCATION=${PKMN_DIR}/${NAMESPACE}${PKMN_NAME}.gif
-	wget http://sprites.pokecheck.org/i/${pkmn_num}.gif \
-		 --quiet --output-document $GIF_LOCATION \
+	GIF_LOCATION="${PKMN_DIR}/${NAMESPACE}${PKMN_NAME}.gif"
+	wget "http://sprites.pokecheck.org/i/${pkmn_num}.gif" \
+		 --quiet --output-document "$GIF_LOCATION" \
 		 --no-clobber
 
 	# Slack has limits of 64KB, and 128x128 pixels
@@ -32,7 +35,7 @@ for pkmn_num in `seq -f "%03g" $1 $2`; do
 		cd ${PKMN_DIR}/exploded
 		gifsicle --unoptimize --disposal=background  --delay=10 --explode full.gif
 		rm full.gif.*[13579] full.gif
-		gifsicle * > halfsize.gif
+		gifsicle ./* > halfsize.gif
 		gifsicle halfsize.gif  --optimize=3 --output halfsize.gif
 		cd ../..
 		mv ${PKMN_DIR}/exploded/halfsize.gif "$GIF_LOCATION"
@@ -43,7 +46,7 @@ for pkmn_num in `seq -f "%03g" $1 $2`; do
 	if [ "$NEW_FILESIZE" -gt "$MAX_FILESIZE" ]; then
 		echo "Filesize still too large, for ${PKMN_NAME} (#${pkmn_num})"
 		echo "Decrease the SAFE_DIMENSION value, or drop more frames"
-		rm $GIF_LOCATION
+		rm "$GIF_LOCATION"
 		exit 1
 	fi
 
